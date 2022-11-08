6007f09c97cb7d2902db08b7ff32ef8cf6f2e53e
0xBAMA <jon.baker1222@gmail.com>
2022-11-08 05:14:09 -0600

    base code, with dependencies

---
+++ src/CMakeFiles/vulkan_guide.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jb/Documents/VkGuideFollowAlong/src/main.cpp" "/home/jb/Documents/VkGuideFollowAlong/src/CMakeFiles/vulkan_guide.dir/main.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/src/vk_engine.cpp" "/home/jb/Documents/VkGuideFollowAlong/src/CMakeFiles/vulkan_guide.dir/vk_engine.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/src/vk_initializers.cpp" "/home/jb/Documents/VkGuideFollowAlong/src/CMakeFiles/vulkan_guide.dir/vk_initializers.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "third_party/vkbootstrap"
+  "third_party/vma"
+  "third_party/glm"
+  "third_party/tinyobjloader"
+  "third_party/imgui"
+  "third_party/SDL_PATH/include/SDL2"
+  "/include/SDL2"
+  "third_party/stb_image"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/vkbootstrap.dir/DependInfo.cmake"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/tinyobjloader.dir/DependInfo.cmake"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ third_party/CMakeFiles/imgui.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/imgui/imgui.cpp" "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/imgui/imgui.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/imgui/imgui_demo.cpp" "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/imgui/imgui_demo.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/imgui/imgui_draw.cpp" "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/imgui/imgui_draw.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/imgui/imgui_impl_sdl.cpp" "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/imgui/imgui_impl_sdl.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/imgui/imgui_impl_vulkan.cpp" "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/imgui/imgui_impl_vulkan.cpp.o"
+  "/home/jb/Documents/VkGuideFollowAlong/third_party/imgui/imgui_widgets.cpp" "/home/jb/Documents/VkGuideFollowAlong/third_party/CMakeFiles/imgui.dir/imgui/imgui_widgets.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "third_party/imgui"
+  "third_party/SDL_PATH/include/SDL2"
+  "/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
