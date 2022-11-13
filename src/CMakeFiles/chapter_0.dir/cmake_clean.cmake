21eae476c2c03e052cbb633f73e913175cea022c
0xBAMA <jon.baker1222@gmail.com>
2022-11-13 17:02:15 -0600

    chapter 0 working

---
+++ src/CMakeFiles/chapter_0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/chapter_0"
+  "../bin/chapter_0.pdb"
+  "CMakeFiles/chapter_0.dir/main.cpp.o"
+  "CMakeFiles/chapter_0.dir/vk_engine.cpp.o"
+  "CMakeFiles/chapter_0.dir/vk_initializers.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
